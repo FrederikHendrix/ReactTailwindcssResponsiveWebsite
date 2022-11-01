3459056894181508b89eaf5277e21fe53c337146
PeopDePelikaan <35820277+PeopDePelikaan@users.noreply.github.com>
2022-11-01 17:29:32 +0100

    Navbar startup

---
+++ data-finance/src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
+        <ul className='flex hidden'>
+            <li className='p-4'>Home</li>
+            <li className='p-4'>Company</li>
+            <li className='p-4'>Resources</li>
+            <li className='p-4'>About</li>
+            <li className='p-4'>Contact</li>
+        </ul>
+        <div>
+            <AiOutlineMenu size={30}/>
+        </div>
+        <div className='fixed left-0 top-0 w-[60%] h-full border-r border-r-white bg-[#000300]'>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-8'>REACT.</h1>
+            <ul className='pt-12 uppercase'>
+                <li className='p-4'>Home</li>
+                <li className='p-4'>Company</li>
+                <li className='p-4'>Resources</li>
+                <li className='p-4'>About</li>
+                <li className='p-4'>Contact</li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
